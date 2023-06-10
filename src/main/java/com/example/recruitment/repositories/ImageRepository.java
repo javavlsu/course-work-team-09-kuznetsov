52d2f3baefff83fc366150b6ba94b104b5f3eac7
Kuznetsov_ <114014421+AndrushaKuznetsov20@users.noreply.github.com>
2023-06-10 13:54:38 +0300

    Delete src directory

--- src/main/java/com/example/recruitment/repositories/ImageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.recruitment.repositories;
-
-import com.example.recruitment.models.Image;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ImageRepository extends JpaRepository<Image, Long> {
-}
--- src/main/java/com/example/recruitment/repositories/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.recruitment.repositories;
-
-import com.example.recruitment.models.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByEmail(String email);
-}
